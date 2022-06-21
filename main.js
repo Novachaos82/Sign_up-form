f56fc83af0a343d8fe38c906347618674b2646c1
Bhavesh Dewangan <ybkdddd@gmail.com>
2022-06-22 05:03:54 +0530

    will add email regexp later

--- main.js
+++ main.js
@@ -34,6 +34,8 @@ function check() {
 
   if (emailValue === "") {
     checkForError(email, "Email cannot be blank");
+  } else if (!checkEmailPattern(emailValue)) {
+    checkForError(email, "error");
   } else {
     checkForSuccess(email);
   }
@@ -46,6 +48,11 @@ function check() {
 
   if (passwordValue === "") {
     checkForError(password, "password cannot be blank");
+  } else if (!checkPassPattern(passwordValue)) {
+    checkForError(
+      password,
+      "Password must contain minimum eight characters, at least one letter and one number"
+    );
   } else {
     checkForSuccess(password);
   }
@@ -54,8 +61,25 @@ function check() {
     checkForError(confirmPassword, "Confirm password cannot be blank");
   } else if (passwordValue !== confirmPasswordValue) {
     checkForError(confirmPassword, "Passwords do not match");
+  } else if (!checkPassPattern(confirmPasswordValue)) {
+    checkForError(
+      confirmPassword,
+      "Password must contain minimum eight characters, at least one letter and one number"
+    );
   } else {
     checkForSuccess(confirmPassword);
+  }
+}
+
+function checkPassword() {
+  const passwordValue = password.value.trim();
+  const confirmPasswordValue = confirmPassword.value.trim();
+  if (passwordValue === confirmPasswordValue) {
+    checkForSuccess(password);
+    checkForSuccess(confirmPassword);
+  } else {
+    checkForError(password, "Passwords does not match");
+    checkForError(confirmPassword, "Passwords does not match");
   }
 }
 
@@ -72,4 +96,30 @@ function checkForSuccess(input) {
   const inputControl = input.parentElement;
 
   inputControl.className = "input-control success";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  {
+    password.addEventListener("keyup", () => {
+      checkPassword();
+      console.log(checkPassword);
+    });
+
+    confirmPassword.addEventListener("keyup", () => {
+      checkPassword();
+      console.log(checkPassword);
+    });
+  }
+});
+
+function checkPassPattern(input) {
+  const re = RegExp("^(?=.*d).{8,}$");
+  console.log(re.test(input));
+  return re.test(input);
+}
+
+function checkEmailPattern(input) {
+  const re = RegExp("^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$");
+  console.log(re.test(input));
+  return re.test(input);
 }
